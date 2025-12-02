94334952e205c9a86a1f2434a54dcdf3faed5ba2
williamwu-mongodb <william.t.wu@mongodb.com>
2025-12-01 16:16:08 -0800

    fix lint
    Signed-off-by: williamwu-mongodb <william.t.wu@mongodb.com>

--- plugins/devtools/src/components/Content/ScheduledTasksContent/ScheduledTasksContent.tsx
+++ plugins/devtools/src/components/Content/ScheduledTasksContent/ScheduledTasksContent.tsx
@@ -21,6 +21,7 @@ import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import TextField from '@material-ui/core/TextField';
+// @ts-ignore
 import debounce from '@material-ui/core/debounce';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { Progress, Table, TableColumn } from '@backstage/core-components';
